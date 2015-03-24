bed6d8d262e73c717c3f9b8dd64f981e74d6fd04
imuntol <imuntol@gmail.com>
2015-03-24 15:40:27 +0700

    Database

--- app/src/main/java/com/earthquake/se/timetodrop/MainActivity.java
+++ app/src/main/java/com/earthquake/se/timetodrop/MainActivity.java
@@ -1,17 +1,27 @@
 package com.earthquake.se.timetodrop;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    SQLiteDatabase mDb;
+    MyDbHelper mHelper;
+    Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHelper = new MyDbHelper(this);
+        mDb = mHelper.getWritableDatabase();
+//        Cursor mCursor = mDb.rawQuery("SELECT * FROM Type", null);
     }
 
 
