1c66dba81a19affdf8a0dfb6f10aa5d76b3923cd
oakkpsw <oakpatsawut@gmail.com>
2015-04-07 00:14:45 +0700

    can delete list view

--- app/src/main/java/com/earthquake/se/timetodrop/firstpage.java
+++ app/src/main/java/com/earthquake/se/timetodrop/firstpage.java
@@ -1,11 +1,15 @@
 package com.earthquake.se.timetodrop;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,22 +17,34 @@ import android.app.ActionBar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.nhaarman.listviewanimations.ArrayAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
+import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.OnItemMovedListener;
+import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.TouchViewDraggableManager;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.OnDismissCallback;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.SimpleSwipeUndoAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.UndoAdapter;
 import com.nhaarman.listviewanimations.appearance.simple.AlphaInAnimationAdapter;
-import com.nhaarman.listviewanimations.appearance.simple.SwingLeftInAnimationAdapter;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class firstpage extends ActionBarActivity {
+    private static final int INITIAL_DELAY_MILLIS = 300;
     FoodDb mHelper;
     SQLiteDatabase mDb;
     Cursor mCursor;
-    ListView listFood;
+    DynamicListView listFood;
+    static ArrayList<String> arr_list = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,24 +52,28 @@ public class firstpage extends ActionBarActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.bar_color)));
         mHelper = new FoodDb(this);
         mDb = mHelper.getReadableDatabase();
-
+        mDb = mHelper.getWritableDatabase();
         mCursor = mDb.rawQuery("SELECT * FROM " + FoodDb.TABLE_NAME2, null);
-        final ArrayList<String> arr_list = new ArrayList<String>();
         mCursor.moveToFirst();
         while (!mCursor.isAfterLast()) {
             arr_list.add("Name : " + mCursor.getString(mCursor.getColumnIndex(mHelper.COL_Name)));
-            for (int i = 1; i < 50; i++) {
-                arr_list.add("Lorem ipsum quis leo pharetra item #" + i);
-            }
+
             mCursor.moveToNext();
 
         }
+        listFood = (DynamicListView) findViewById(R.id.listFood);
+        ArrayAdapter<String> adapterDir = new MyListAdapter(this);
+        SimpleSwipeUndoAdapter simpleSwipeUndoAdapter = new SimpleSwipeUndoAdapter(adapterDir, this, new MyOnDismissCallback(adapterDir));
+        AlphaInAnimationAdapter animAdapter = new AlphaInAnimationAdapter(simpleSwipeUndoAdapter);
+        animAdapter.setAbsListView(listFood);
+        assert animAdapter.getViewAnimator() != null;
+        animAdapter.getViewAnimator().setInitialDelayMillis(INITIAL_DELAY_MILLIS);
+        listFood.setAdapter(animAdapter);
+        listFood.enableSimpleSwipeUndo();
 
-        ArrayAdapter<String> adapterDir = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr_list);
-        SwingLeftInAnimationAdapter animationAdapter = new SwingLeftInAnimationAdapter(adapterDir);
-        listFood = (ListView) findViewById(R.id.listFood);
-        animationAdapter.setAbsListView(listFood);
-        listFood.setAdapter(animationAdapter);
+
+
+
 
     }
     public void onStop() {
@@ -91,4 +111,85 @@ public class firstpage extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
+    private static class MyListAdapter extends ArrayAdapter<String> implements UndoAdapter {
+
+        private final Context mContext;
+
+        MyListAdapter(final Context context) {
+            mContext = context;
+            for (int i = 0; i < arr_list.size(); i++) {
+                add(arr_list.get(i));
+            }
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            return getItem(position).hashCode();
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getView(final int position, final View convertView, final ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = LayoutInflater.from(mContext).inflate(R.layout.list_row_dynamiclistview, parent, false);
+            }
+
+            ((TextView) view.findViewById(R.id.list_row_draganddrop_textview)).setText(getItem(position));
+
+            return view;
+        }
+
+        @NonNull
+        @Override
+        public View getUndoView(final int position, final View convertView, @NonNull final ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = LayoutInflater.from(mContext).inflate(R.layout.undo_row, parent, false);
+            }
+            return view;
+        }
+
+        @NonNull
+        @Override
+        public View getUndoClickView(@NonNull final View view) {
+            return view.findViewById(R.id.undo_row_undobutton);
+        }
+    }
+    private class MyOnDismissCallback implements OnDismissCallback {
+
+        private final ArrayAdapter<String> mAdapter;
+
+        @Nullable
+        private Toast mToast;
+
+        MyOnDismissCallback(final ArrayAdapter<String> adapter) {
+            mAdapter = adapter;
+        }
+
+        @Override
+        public void onDismiss(@NonNull final ViewGroup listView, @NonNull final int[] reverseSortedPositions) {
+            for (int position : reverseSortedPositions) {
+                mAdapter.remove(position);
+            }
+
+            if (mToast != null) {
+                mToast.cancel();
+            }
+            mToast = Toast.makeText(
+                    firstpage.this,
+                    getString(R.string.removed_positions, Arrays.toString(reverseSortedPositions)),
+                    Toast.LENGTH_LONG
+            );
+            mToast.show();
+
+        }
+
+    }
+
 }
